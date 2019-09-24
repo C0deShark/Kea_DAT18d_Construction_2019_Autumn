352b9c0a51181fc6719fc606f90b535f44dff258
anders <anderslatif@gmail.com>
2019-09-24 08:41:38 +0200

    Added example of a synchronised block

--- 3._Threads/src/Main.java
+++ 3._Threads/src/Main.java
@@ -30,10 +30,19 @@ public class Main {
         // implements Runnable
         ThreadImplementsRunnable runnableThread = new ThreadImplementsRunnable();
         Thread thread = new Thread(threadGroup, runnableThread);
-        thread.start();
+        // thread.start();
 
         // System.out.println(thread.getThreadGroup().getName());
 
+        PrintThreads thread0 = new PrintThreads("En lang saetning, en laengere saetning, en rigtig lang saetning");
+        PrintThreads thread1 = new PrintThreads("11111111111111111111111111111111111111111111111111111111111111");
+        PrintThreads thread2 = new PrintThreads("22222222222222222222222222222222222222222222222222222222222222");
+        PrintThreads thread3 = new PrintThreads("Koerne spiser rigtig rigtig rigtig meget graes");
+
+        thread0.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
 
     }
