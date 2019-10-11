0eb0521ba7f1d4611a756b8885603c88f2a44717
anders <anderslatif@gmail.com>
2019-10-11 11:47:53 +0200

    Implemented first part of the CreamController

--- 7._Cream_API/src/main/java/edu/kea/andl/cream/controller/CreamController.java
+++ 7._Cream_API/src/main/java/edu/kea/andl/cream/controller/CreamController.java
@@ -1,4 +1,36 @@
 package edu.kea.andl.cream.controller;
 
+import edu.kea.andl.cream.model.Cream;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
 public class CreamController {
+    ArrayList<Cream> cream = new ArrayList<>();
+
+    @RequestMapping(value = "/cream", method = RequestMethod.GET)
+    public String getCream() {
+        String response = "";
+        String newLine = "\r\n";
+        for (Cream c : cream) {
+            response += c.name + newLine;
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "/cream", method = RequestMethod.POST)
+    public String postCream(@RequestBody String body) {
+        return body;
+    }
+
+    @RequestMapping(value = "/cream/{id}", method = RequestMethod.DELETE)
+    public String deleteCream(@PathVariable int id) {
+        if (id >= 0 && id < cream.size()) {
+            cream.remove(id);
+            return "Cream deleted";
+        } else {
+            return "Cream was not deleted, out of bounds.";
+        }
+    }
 }
--- 7._Cream_API/src/main/java/edu/kea/andl/cream/model/Cream.java
+++ 7._Cream_API/src/main/java/edu/kea/andl/cream/model/Cream.java
@@ -1,4 +1,9 @@
 package edu.kea.andl.cream.model;
 
 public class Cream {
+    public String name;
+
+    public Cream(String name) {
+        this.name = name;
+    }
 }
