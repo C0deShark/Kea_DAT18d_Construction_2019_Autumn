8d4b37cd28d5eb3dffd1a633087037cdbb426bd6
anders <anderslatif@gmail.com>
2019-10-15 15:00:12 +0200

    Added Cream task in the Cream model and created the project ideas file

--- 7._Cream_API/src/main/java/edu/kea/andl/cream/model/Cream.java
+++ 7._Cream_API/src/main/java/edu/kea/andl/cream/model/Cream.java
@@ -3,6 +3,13 @@ package edu.kea.andl.cream.model;
 public class Cream {
     public String name;
 
+    // todo add: createdBy, bestBy and isExpired
+    // createdBy is a timestamp
+    // bestBy: add 1 week to createdBy
+    // isExpired: true if now is later than bestBy
+            // hint: ternary statement
+
+
     public Cream(String name) {
         this.name = name;
     }
